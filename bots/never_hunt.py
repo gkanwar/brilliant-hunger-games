fc0cce5f6e93875ca96d1fd063cc29ce40960109
Gurtej Kanwar <gurtej@mit.edu>
2013-07-20 12:40:37 -0700

    Adds initial files

---
+++ bots/never_hunt.py
@@ -0,0 +1,11 @@
+def hunt_choices(round_number, current_food, current_reputation, m,
+            player_reputations):
+
+    hunt_decisions = ['s' for x in player_reputations]
+    return hunt_decisions
+
+def hunt_outcomes(food_earnings):
+    pass # do nothing
+
+def round_end(award, m, number_hunters):
+    pass # do nothing
