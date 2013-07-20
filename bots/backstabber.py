ea0b776386506ca56e280aa89b9423f01749be8d
neilfitz <fitzgerald.neil@gmail.com>
2013-07-20 13:32:46 -0700

    Added backstabber, minor modifications

--- bots/backstabber.py
+++ bots/backstabber.py
@@ -2,14 +2,15 @@ def hunt_choices(round_number, current_food, current_reputation, m,
             player_reputations):
 
     avg_reputation = float(sum(player_reputations))/len(player_reputations)
-    players_to_trust = avg_reputation * len(player_reputations)
 
-    reps = list(player_reputations)
+    reps = list(avg_reputation)
     reps.sort()
-    rep_to_trust = reps[players_to_trust]
+    sucker = reps[int(0.9 * len(player_reputations))]
     
     hunt_decisions = list()
     for reputation in player_reputations:
+        if reputation >= sucker:
+            hunt_decisions.append('s')
         if reputation >= rep_to_trust:
             hunt_decisions.append('h')
         else:
