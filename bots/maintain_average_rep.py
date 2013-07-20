d4f3115c70fb4dcb5211fe4d971f7be908df8f98
Gurtej Kanwar <gurtej@mit.edu>
2013-07-20 14:40:40 -0700

    Improves maintain average rep

--- bots/maintain_average_rep.py
+++ bots/maintain_average_rep.py
@@ -1,18 +1,24 @@
+REP_MULTIPLIER = 1.25
+
 def hunt_choices(round_number, current_food, current_reputation, m,
-            player_reputations):
+                 player_reputations):
 
     avg_reputation = float(sum(player_reputations))/len(player_reputations)
-    players_to_trust = avg_reputation * len(player_reputations)
+    players_to_trust = min(REP_MULTIPLIER * (avg_reputation * len(player_reputations)), len(player_reputations))
 
     reps = list(player_reputations)
-    reps.sort()
-    reps.reverse()
-    rep_to_trust = reps[int(players_to_trust)]
+    reps.sort(reverse=True)
+    rep_to_trust = reps[int(players_to_trust-1)]
+    num_above_rep_to_trust = len([r for r in reps if r > rep_to_trust])
+    num_equal_rep_to_trust = players_to_trust - num_above_rep_to_trust
 
     hunt_decisions = list()
     for reputation in player_reputations:
-        if reputation >= rep_to_trust:
+        if reputation > rep_to_trust:
             hunt_decisions.append('h')
+        elif reputation == rep_to_trust and num_equal_rep_to_trust > 0:
+            hunt_decisions.append('h')
+            num_equal_rep_to_trust -= 1
         else:
             hunt_decisions.append('s')
     return hunt_decisions
